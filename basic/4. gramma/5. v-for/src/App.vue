857a6535e4fedd06495e9f8c2ca42303f55ca292
gimtaehwan <gimtaehwan8246@gmail.com>
2026-03-23 17:10:07 +0900

    중요개념

--- basic/4. gramma/5. v-for/src/App.vue
+++ basic/4. gramma/5. v-for/src/App.vue
@@ -2,20 +2,22 @@
   <div style="width: 200px">
     <ol>
       <li v-for="item in items">{{ item }}</li>
+      <!-- item 배열 안에 있는 객체들 하나씩 꺼내서 출력 -->
     </ol>
   </div>
 </template>
 
 <script>
-import { reactive } from 'vue'
+import { reactive } from 'vue';
 
 export default {
   // Composition API
   setup() {
-    const items = reactive({ 1: '1번 객체', 2: '2번 객체', 3: '3번 객체' })
+    //ref() 없으니까 반응형 아님
+    const items = reactive({ 1: '1번 객체', 2: '2번 객체', 3: '3번 객체' });
     return {
-      items,
-    }
+      items, // = items++
+    };
   },
-}
+};
 </script>
