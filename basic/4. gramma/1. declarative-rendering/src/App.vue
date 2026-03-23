d68f9ba83da1d50019411e1344184bea3539ac20
gimtaehwan <gimtaehwan8246@gmail.com>
2026-03-23 16:41:17 +0900

    중요개념

--- basic/4. gramma/1. declarative-rendering/src/App.vue
+++ basic/4. gramma/1. declarative-rendering/src/App.vue
@@ -10,18 +10,20 @@
 <script>
 export default {
   // Composition API
+  //변수를 만들고 상수형으로 데이터 입력(반응형 X), return 해야 사용가능
   setup() {
-    const date = Date().toString()
+    const date = Date().toString();
     return {
       date,
-    }
+    };
   },
 
   // Option API
+  //자동으로 반영됨
   data() {
     return {
       date2: Date().toString(),
-    }
+    };
   },
-}
+};
 </script>
